44ae8cb22118ba538c7a44986226d4453b204004
haisndu <thirasa-im21062@stu.kln.ac.lk>
2025-05-09 15:30:52 +0530

    litile update

--- frontend/src/pages/RetreadingDetails.js
+++ frontend/src/pages/RetreadingDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { getRetreadingDetails } from "../services/retreadingService";
+import { getRetreadingDetails } from "../services/orderServices";
 import "../styles/RetreadingDetails.css";
 
 const RetreadingDetails = () => {
